c5a6e428a08bd2fd2b8c35d7ee13531bcf5501c3
dadenusto2 <57599004+dadenusto2@users.noreply.github.com>
2021-10-20 12:58:06 +0300

    20.10.2021

---
+++ App3/app/src/main/java/com/example/mylists/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.mylists;
+
+//фио студентов и тд
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private int mPosition;
+    private ActivityResultLauncher<Intent> mIntentActivityResultLauncher;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.miAbout:{
+                AlertDialog.Builder infoDialog = new AlertDialog.Builder(MainActivity.this);
+                infoDialog.setTitle("О программе");
+                infoDialog.setMessage("Это программа группы 4ИТ/2!");
+                infoDialog.setCancelable(false);
+                infoDialog.setPositiveButton("Прочитано", null);
+                infoDialog.show();
+                return true;
+
+            }
+            case R.id.miExit:{
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_ll);
+
+        ((LinearLayout) findViewById(R.id.llInput)).setVisibility(
+                ((Button) findViewById(R.id.bAddStudent)).getVisibility()
+        );
+
+        mIntentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK) {
+                            Intent intent = result.getData();
+                            Student s = intent.getParcelableExtra("student");
+                            mStudents.set(mPosition, s);
+                            Toast.makeText(getApplicationContext(),
+                                    "Студент: " + s.toString() + "\nУспешно сохранён", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+
+    public void createList1(View view){
+        String[] catNames = new String[]{
+                "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
+                "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+                "Китти", "Масяня", "Симба"
+        };
+        Arrays.sort(catNames);
+
+        ListView listView = findViewById(R.id.lvList1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, catNames);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Выбрано: "+((TextView) view).getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    ArrayList<Student> mStudents;
+    StudentListAdapter mStudentListAdapter;
+
+    public void createStudentList(View view) {
+        mStudents=new ArrayList<>();
+        ListView listView = findViewById(R.id.lvList2);
+        mStudentListAdapter=new StudentListAdapter(mStudents,this);
+        listView.setAdapter(mStudentListAdapter);
+
+        ((LinearLayout) findViewById(R.id.llInput)).setVisibility(View.VISIBLE);
+        ((Button) findViewById(R.id.bAddStudent)).setVisibility(View.VISIBLE);
+        ((Button) findViewById(R.id.bCreateStudentList)).setVisibility(View.GONE);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mStudentListAdapter.colorFaculty(position, parent);
+            }
+        });
+
+        AdapterView.OnItemClickListener clStudent = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, StudentInfoActivity.class);
+
+//                intent.putExtra("fio", mStudents.get(position).getFIO());
+//                intent.putExtra("faculty", mStudents.get(position).getFaculty());
+//                intent.putExtra("group", mStudents.get(position).getGroup());
+
+//                Bundle bundle = new Bundle();
+//                bundle.putString("fio", mStudents.get(position).getFIO());
+//                bundle.putString("faculty", mStudents.get(position).getFaculty());
+//                bundle.putString("group", mStudents.get(position).getGroup());
+//
+//                intent.putExtras(bundle);
+                intent.putExtra("student", mStudents.get(position));
+                mPosition=position;
+                //startActivity(intent);
+                mIntentActivityResultLauncher.launch(intent);
+            }
+        };
+        listView.setOnItemClickListener(clStudent);
+    }
+
+    public void addStudent(View view) {
+        mStudents.add(new Student(
+                ((EditText) findViewById(R.id.editFIO)).getText().toString(),
+                ((EditText) findViewById(R.id.editFaculty)).getText().toString(),
+                ((EditText) findViewById(R.id.editGroup)).getText().toString()
+        ));
+        ((EditText) findViewById(R.id.editFIO)).setText("");
+        //((EditText) findViewById(R.id.editFaculty)).setText("");
+        //((EditText) findViewById(R.id.editGroup)).setText("");
+        mStudentListAdapter.notifyDataSetChanged();
+    }
+}
---
+++ App3/app/src/main/java/com/example/mylists/StudentInfoActivity.java
@@ -0,0 +1,140 @@
+package com.example.mylists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class StudentInfoActivity extends AppCompatActivity {
+
+    private SubjectListAdapter mSubjectListAdapter;
+    private Student s;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_info);
+
+//        ((TextView) findViewById(R.id.tvElementFIO)).setText(getIntent().getStringExtra("fio"));
+//        ((TextView) findViewById(R.id.tvElementFaculty)).setText(getIntent().getStringExtra("faculty"));
+//        ((TextView) findViewById(R.id.tvElementGroup)).setText(getIntent().getStringExtra("group"));
+
+//        Bundle bundle = getIntent().getExtras();
+//        ((TextView) findViewById(R.id.tvElementFIO)).setText(bundle.getString("fio"));
+//        ((TextView) findViewById(R.id.tvElementFaculty)).setText(bundle.getString("faculty"));
+//        ((TextView) findViewById(R.id.tvElementGroup)).setText(bundle.getString("group"));
+
+        s = getIntent().getParcelableExtra("student");
+        ((TextView) findViewById(R.id.tvASI_FIO)).setText(s.getFIO());
+        ((TextView) findViewById(R.id.tvASI_Faculty)).setText(s.getFaculty());
+        ((TextView) findViewById(R.id.tvASI_Group)).setText(s.getGroup());
+
+        mSubjectListAdapter = new SubjectListAdapter(s.getSubjects(),StudentInfoActivity.this);
+        ((ListView) findViewById(R.id.lvASI_Subjects)).setAdapter(mSubjectListAdapter);
+
+
+        ((ListView) findViewById(R.id.lvASI_Subjects)).setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                AlertDialog.Builder quitDialog = new AlertDialog.Builder(StudentInfoActivity.this);
+                quitDialog.setTitle("Удалить дисцеплину\"" + s.getSubjects().get(i).getName()+"\"?");
+
+                quitDialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        s.getSubjects().remove(i);
+                        mSubjectListAdapter.notifyDataSetChanged();
+                    }
+                })
+                        .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        });
+                quitDialog.show();
+                return false;
+            }
+        });
+        ((ListView) findViewById(R.id.lvASI_Subjects)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showPopupMenu(view, i);
+            }
+        });
+    }
+
+    public void showPopupMenu(View view, int position) {
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Integer mark;
+                switch (item.getItemId()) {
+                    case R.id.popup5: {mark = 5; break;}
+                    case R.id.popup4: {mark = 4; break;}
+                    case R.id.popup3: {mark = 3; break;}
+                    case R.id.popup2: {mark = 2; break;}
+                    default:
+                        return false;
+                }
+                if ((s!=null)&&(position<s.getSubjects().size())){
+                    s.getSubjects().get(position).setMark(mark);
+                    mSubjectListAdapter.notifyDataSetChanged();
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
+
+    public void addSubject(View view) {
+        s.addSubject(new Subject(
+                ((EditText) findViewById(R.id.editASI_SubjectName)).getText().toString(),
+                Integer.parseInt(((Spinner) findViewById(R.id.sASI_mark)).getSelectedItem().toString())
+        ));
+        mSubjectListAdapter.notifyDataSetChanged();
+    }
+
+    public void clSave(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("student",s);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void clExit(View view) {
+        finish();
+    }
+
+    public void onBackPassed() {
+        AlertDialog.Builder quitDialog = new AlertDialog.Builder(this);
+        quitDialog.setTitle("Сохранить изменения?");
+        quitDialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                clSave(null);
+            }
+        })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        clExit(null);
+                    }
+                });
+        quitDialog.show();
+    }
+
+
+}
